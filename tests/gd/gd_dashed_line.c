0718bed262bda7b9113eeb1d260858aafb6f4eb7
willson-chen <willson.chenwx@gmail.com>
2019-11-04 19:19:56 +0800

    add testcase of gdImageDashedline

---
+++ tests/gd/gd_dashed_line.c
@@ -0,0 +1,25 @@
+#include "gd.h"
+#include "gdtest.h"
+
+int main()
+{
+gdImagePtr im;
+	im = gdImageCreate(64,64);
+	int white;
+
+
+	gdImageColorAllocate(im, 0, 0, 0);
+	white = gdImageColorAllocate(im, 255, 255, 255);
+	gdImageDashedLine(im, 0, 64, 64, 0, white);
+	gdImageDashedLine(im, 0, 0, 10, 63, white);
+	//FILE *pngout;
+	//pngout = fopen("gd/test.png", "wb");
+	//pngout = gdTestFileOpen2("gd", "test.png");
+	//gdImagePng(im, pngout);
+	//fclose(pngout);
+
+	gdAssertImageEqualsToFile("gd/test.png", im);
+
+	gdImageDestroy(im);
+	return 0;
+}
